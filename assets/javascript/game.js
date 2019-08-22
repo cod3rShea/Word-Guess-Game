2eb92af8b50bd792a5eeb0f81a2da4e9c0a6cde6
Sean Michael Shea <seanshea1337@gmail.com>
2019-08-21 21:57:40 -0400

    add some comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,9 @@ var wrongLetters = [];
 // Band Names array
 var bandNames = ["AJJ", "Bob Dylan", "cat be damned", "Coma Cinema", "David Bowie", "Days N Daze", "elvis depressedly", "HIM", "Hotel Books", "Johnny Hobo And The Freight Trains", "mathew lee cothranm", "My Chemical Romance", "My Pizza My World", "Old Gray", "Ramshackle Glory", "teen suicide", "The Front Bottoms", "The Hotelier"]
 
-// picks a random band from the band array
+// creates a random number between 1 and the length of the band array.
 var randomNumber = Math.floor(Math.random() * bandNames.length);
+// picks a random band based of the length of the randomband array
 var selectedBand = bandNames[randomNumber];
 
 console.log(selectedBand);
