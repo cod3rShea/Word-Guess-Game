eee4eb2f2c48995a1edf4b05e4884ae42af434e2
Sean Michael Shea <seanshea1337@gmail.com>
2019-08-21 20:59:42 -0400

    first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var bandNames = ["AJJ", "Bob Dylan", "cat be damned", "Coma Cinema", "David Bowie", "Days N Daze", "elvis depressedly", "HIM", "Hotel Books", "Johnny Hobo And The Freight Trains", "mathew lee cothranm", "My Chemical Romance", "My Pizza My3 World", "Old Gray", "Ramshackle Glory", "teen suicide", "The Front Bottoms", "The Hotelier"]
+
+for (i = 0; i < bandNames.length; i++) {
+    console.log(bandNames[i]);
+}
