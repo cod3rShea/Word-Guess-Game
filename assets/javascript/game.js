c07248ad6a553d2881142b555731704b83ed5916
Sean Michael Shea <seanshea1337@gmail.com>
2019-08-21 21:44:41 -0400

    create span by on band name length and append to current work container

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,20 @@ var correctLetters = [];
 var wrongLetters = [];
 
 // Band Names array
-var bandNames = ["AJJ", "Bob Dylan", "cat be damned", "Coma Cinema", "David Bowie", "Days N Daze", "elvis depressedly", "HIM", "Hotel Books", "Johnny Hobo And The Freight Trains", "mathew lee cothranm", "My Chemical Romance", "My Pizza My3 World", "Old Gray", "Ramshackle Glory", "teen suicide", "The Front Bottoms", "The Hotelier"]
+var bandNames = ["AJJ", "Bob Dylan", "cat be damned", "Coma Cinema", "David Bowie", "Days N Daze", "elvis depressedly", "HIM", "Hotel Books", "Johnny Hobo And The Freight Trains", "mathew lee cothranm", "My Chemical Romance", "My Pizza My World", "Old Gray", "Ramshackle Glory", "teen suicide", "The Front Bottoms", "The Hotelier"]
 
 // picks a random band from the band array
 var randomNumber = Math.floor(Math.random() * bandNames.length);
 var selectedBand = bandNames[randomNumber];
 
 console.log(selectedBand);
+console.log(bandNames[randomNumber].length);
+
+for (i = 0; i < bandNames[randomNumber].length; i++) {
+    var underlineSpan = document.createElement("span"); 
+    document.getElementById("current-word").appendChild(underlineSpan);
+}
+
 
 
 
