bd3149cfe3d3b6560d827291309f5810c19cf82f
Sean Michael Shea <seanshea1337@gmail.com>
2019-08-21 21:29:21 -0400

    add bands array.. add empty correct and wrong array... select random band

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,19 @@
+/*********************** 
+    Arrays
+************************/
+var correctLetters = [];
+var wrongLetters = [];
+
+// Band Names array
 var bandNames = ["AJJ", "Bob Dylan", "cat be damned", "Coma Cinema", "David Bowie", "Days N Daze", "elvis depressedly", "HIM", "Hotel Books", "Johnny Hobo And The Freight Trains", "mathew lee cothranm", "My Chemical Romance", "My Pizza My3 World", "Old Gray", "Ramshackle Glory", "teen suicide", "The Front Bottoms", "The Hotelier"]
 
-for (i = 0; i < bandNames.length; i++) {
-    console.log(bandNames[i]);
-}
+// picks a random band from the band array
+var randomNumber = Math.floor(Math.random() * bandNames.length);
+var selectedBand = bandNames[randomNumber];
+
+console.log(selectedBand);
+
+
+
+
+
